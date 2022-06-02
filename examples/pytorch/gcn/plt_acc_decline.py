9ca5dc8877b604b1b2a079d37e606da6fae3abcb
Chen Yin <yinchen0621@foxmail.com>
2022-06-02 23:28:47 +0800

    Drop the last time epoch for there is no inserted node

--- examples/pytorch/gcn/plt_acc_decline.py
+++ examples/pytorch/gcn/plt_acc_decline.py
@@ -3,24 +3,34 @@ import numpy as np
 
 
 def plt_edge_epoch():
-    cora = np.loadtxt('./results/cora_add_edge.txt', delimiter=',')
-    citeseer = np.loadtxt('./results/citeseer_add_edge.txt', delimiter=',')
-    pubmed = np.loadtxt('./results/pubmed_add_edge.txt', delimiter=',')
+    cora = np.loadtxt('./results/cora_add_edge_deg_30.txt', delimiter=',')
+    citeseer = np.loadtxt('./results/citeseer_add_edge_deg_30.txt', delimiter=',')
+    pubmed = np.loadtxt('./results/pubmed_add_edge_deg_30.txt', delimiter=',')
 
-    # x1 = cora[:, 0]
-    # x2 = citeseer[:, 0]
-    # x3 = pubmed[:, 0]
-    x1 = range(len(cora))
-    x2 = range(len(citeseer))
-    x3 = range(len(pubmed))
+    epoch_num_cora = cora.shape[0] - 1
+    epoch_num_citeseer = citeseer.shape[0] - 1
+    epoch_num_pubmed = pubmed.shape[0] - 1
+
+    # Drop the last epoch for no inserted node
+    epoch_num_cora -= 1
+    epoch_num_citeseer -= 1
+    epoch_num_pubmed -= 1
+
+    x1 = range(epoch_num_cora)
+    x2 = range(epoch_num_citeseer)
+    x3 = range(epoch_num_pubmed)
+
+    print(x1)
+    print(cora[:epoch_num_cora, -1])
+    print(range(10))
 
     fig = plt.figure(figsize=(10, 3), dpi=100)
 
     ax1 = plt.subplot(1, 3, 1)
     # plt.plot(x1, cora[:, -2], 'v-', color='g', label="Delta")
-    plt.plot(x1, cora[:, -1], 'o-', color='b', label="Delta retrain")
-    plt.plot(x1, cora[:, -3], 'o-', color='r', label="Full graph retrain")
-    plt.plot(x1, cora[:, -4], 'o-', color='grey', label="W/O retrain")
+    plt.plot(x1, cora[:epoch_num_cora, -1], 'o-', color='b', label="Delta retrain")
+    plt.plot(x1, cora[:epoch_num_cora, -3], 'o-', color='r', label="Full graph retrain")
+    plt.plot(x1, cora[:epoch_num_cora, -4], 'o-', color='grey', label="W/O retrain")
     # plt.legend(prop={'size': 10})
     ax1.set_ylim([0, 100])
     plt.ylabel('Accuracy (%)', fontsize=16)
@@ -28,9 +38,9 @@ def plt_edge_epoch():
 
     ax2 = plt.subplot(1, 3, 2)
     # plt.plot(x2, citeseer[:, -2], 'v-', color='g', label="Delta")
-    plt.plot(x2, citeseer[:, -1], 'v-', color='b', label="Delta retrain")
-    plt.plot(x2, citeseer[:, -3], 'o-', color='r', label="Full graph retrain")
-    plt.plot(x2, citeseer[:, -4], 'o-', color='grey', label="W/O retrain")
+    plt.plot(x2, citeseer[:epoch_num_citeseer, -1], 'v-', color='b', label="Delta retrain")
+    plt.plot(x2, citeseer[:epoch_num_citeseer, -3], 'o-', color='r', label="Full graph retrain")
+    plt.plot(x2, citeseer[:epoch_num_citeseer, -4], 'o-', color='grey', label="W/O retrain")
     # plt.legend(prop={'size': 10})
     ax2.set_ylim([0, 100])
     # plt.ylabel('Accuracy (%)', fontsize=16)
@@ -38,9 +48,9 @@ def plt_edge_epoch():
 
     ax3 = plt.subplot(1, 3, 3)
     # plt.plot(x3, pubmed[:, -2], 'v-', color='g', label="Delta")
-    plt.plot(x3, pubmed[:, -1], 'v-', color='b', label="Delta retrain")
-    plt.plot(x3, pubmed[:, -3], 'o-', color='r', label="Full graph retrain")
-    plt.plot(x3, pubmed[:, -4], 'o-', color='grey', label="W/O retrain")
+    plt.plot(x3, pubmed[:epoch_num_pubmed, -1], 'v-', color='b', label="Delta retrain")
+    plt.plot(x3, pubmed[:epoch_num_pubmed, -3], 'o-', color='r', label="Full graph retrain")
+    plt.plot(x3, pubmed[:epoch_num_pubmed, -4], 'o-', color='grey', label="W/O retrain")
     plt.legend(prop={'size': 12})
     ax3.set_ylim([0, 100])
     # plt.ylabel('Accuracy (%)', fontsize=16)
