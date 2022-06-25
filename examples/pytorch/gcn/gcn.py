Refactor GCN-delta to fix up weight and bias detaching from computation graph

examples/pytorch/gcn/gcn.py
@@ -6,6 +6,7 @@ References:
 - Code: https://github.com/tkipf/gcn
 """
 import torch
+import torch as th
 import torch.nn as nn
 from dgl.nn.pytorch import GraphConv
 from graphconv_delta import GraphConv_delta
@@ -63,25 +64,14 @@ class GCN_delta(nn.Module):
         self.layers.append(GraphConv_delta(n_hidden, n_classes))
         self.dropout = nn.Dropout(p=dropout)
 
-    # def forward(self, features):
-    #     h = features
-    #     # h1 = h
-    #     for i, layer in enumerate(self.layers):
-    #         if i != 0:
-    #             h = self.dropout(h)
-    #         h = layer(self.g, h)
+        # # Record whether a node has been inferenced embedding
+        # self.nodes_has_inferenced_mask = [0 for i in range(g.number_of_nodes())]
 
-    #         # h1 = torch.matmul(h1, layer.weight)
-    #         # print(h.equal(h1))
-    #     return h
-
-    # def forward(self, features, nodes_high_deg=None, nodes_low_deg=None):
-    #     h = features
-    #     for i, layer in enumerate(self.layers):
-    #         if i != 0:
-    #             h = self.dropout(h)
-    #         h = layer(self.g, h, nodes_high_deg, nodes_low_deg)
-    #     return h
+        # Record previous embedding
+        # self.embedding = nn.Parameter(
+        #     th.Tensor([[0 for i in range(n_classes)] for j in range(g.number_of_nodes())]))
+        self.embedding = th.Tensor([[0 for i in range(n_classes)]
+                                    for j in range(g.number_of_nodes())]).requires_grad_(True)
 
     def forward(self, features, ngh_high_deg=None, ngh_low_deg=None):
         h = features
@@ -89,4 +79,65 @@ class GCN_delta(nn.Module):
             if i != 0:
                 h = self.dropout(h)
             h = layer(self.g, h, ngh_high_deg, ngh_low_deg)
+
+        # for name, parameters in self.named_parameters():
+        #     print(name, ':', parameters.size())
+        #     print(parameters.detach().is_leaf)
+        #     print(parameters.detach().grad)
+        #     print(parameters.detach().grad_fn)
+
+        if (ngh_high_deg is not None) or (ngh_low_deg is not None):
+            # Combine delta-inferenced embedding and previous embedding
+            h = self.combine_embedding(self.embedding, h, ngh_high_deg, ngh_low_deg)
+
         return h
+
+    def combine_embedding(self, embedding_prev, feat, ngh_high_deg, ngh_low_deg):
+        # Combine delta rst with feat_prev
+        feat_prev_ind = list(i for i in range(embedding_prev.shape[0]))
+        feat_prev_keep_ind = list(set(feat_prev_ind) - set(ngh_high_deg) - set(ngh_low_deg))
+
+        feat_prev_keep_ind = th.tensor(feat_prev_keep_ind, dtype=th.long)
+        # ngh_high_deg_ind = th.tensor(ngh_high_deg, dtype=th.long)
+        ngh_low_deg_ind = th.tensor(ngh_low_deg, dtype=th.long)
+
+        feat_prev = th.index_select(embedding_prev, 0, feat_prev_keep_ind)
+        # feat_high_deg = th.index_select(feat, 0, ngh_high_deg_ind)
+        feat_low_deg = th.index_select(feat, 0, ngh_low_deg_ind)
+
+        # Gen index for scatter
+        index_feat_prev = [[feat_prev_keep_ind[row].item() for col in range(feat_prev.shape[1])]
+                           for row in range(feat_prev_keep_ind.shape[0])]
+        # index_high_deg = [[ngh_high_deg_ind[row].item() for col in range(feat_high_deg.shape[1])]
+        #                   for row in range(ngh_high_deg_ind.shape[0])]
+        index_low_deg = [[ngh_low_deg_ind[row].item() for col in range(feat_low_deg.shape[1])]
+                         for row in range(ngh_low_deg_ind.shape[0])]
+
+        index_feat_prev = th.tensor(index_feat_prev)
+        # index_high_deg = th.tensor(index_high_deg)
+        index_low_deg = th.tensor(index_low_deg)
+
+        # Update feat of the nodes in the high and low degree
+        feat.scatter(0, index_feat_prev, feat_prev)
+        # embedding_prev.scatter(0, index_high_deg, feat_high_deg)
+        feat.scatter(0, index_low_deg, feat_low_deg, reduce='add')
+
+        return feat
+
+    # def resize_embedding(self, embedding, feat):
+    #     # Resize feat_prev
+    #     feat_prev_num = embedding.shape[0]
+    #     feat_updated_num = feat.shape[0]
+    #     # print(embedding.grad, feat.grad)
+    #     if feat_prev_num < feat_updated_num:
+    #         added_feat_num = feat_updated_num - feat_prev_num
+    #         tmp = th.zeros(added_feat_num, embedding.shape[1], requires_grad=True)
+    #         embedding = th.cat((embedding, tmp), 0).requires_grad_(True)
+    #         embedding.retain_grad()
+    #         print('\n', embedding.grad)
+    #         # tmp = th.ones(feat_updated_num,
+    #         #               embedding.shape[1],
+    #         #               dtype=th.float32,
+    #         #               requires_grad=True)
+    #         # embedding_resize = th.mul(embedding, tmp)
+    #     return embedding

examples/pytorch/gcn/graphconv_delta.py
@@ -27,55 +27,16 @@ class GraphConv_delta(GraphConv):
                                               allow_zero_in_degree)
 
         # feat_prev size is variable with graph evolving (size = g_nodes_number * out_feats)
-        self.feat = nn.Parameter(th.Tensor(1, out_feats))
+        # self.feat = nn.Parameter(th.Tensor(1, out_feats))
 
     def forward(self, graph, feat, ngh_high_deg=None, ngh_low_deg=None):
         r"""    
         The data type of "ngh_high_deg" and "ngh_low_deg" is List
         """
-        if ngh_high_deg is not None and ngh_low_deg is not None:
-            rst_golden = super().forward(graph, feat)
-
+        if (ngh_high_deg is not None) or (ngh_low_deg is not None):
             edge_mask = graph.edata['edge_mask']
             rst_delta = super().forward(graph, feat, edge_weight=edge_mask)
-
-            # Resize feat_prev
-            feat_num = self.feat.shape[0]
-            if self.feat.shape[0] < rst_delta.shape[0]:
-                new_feat_num = rst_delta.shape[0] - self.feat.shape[0]
-                self.feat = nn.Parameter(
-                    th.cat((self.feat, th.zeros([new_feat_num, self.feat.shape[1]])), 0))
-
-            # Combine delta rst with feat_prev
-            ngh_high_deg_ind = th.tensor(ngh_high_deg, dtype=th.long)
-            ngh_low_deg_ind = th.tensor(ngh_low_deg, dtype=th.long)
-
-            feat_high_deg = th.index_select(rst_delta, 0, ngh_high_deg_ind)
-            feat_high_deg_golden = th.index_select(rst_golden, 0, ngh_high_deg_ind)
-
-            feat_low_deg = th.index_select(rst_delta, 0, ngh_low_deg_ind)
-            feat_low_deg_golden = th.index_select(rst_golden, 0, ngh_low_deg_ind)
-            feat_low_deg_updated_golden = feat_low_deg_golden + feat_low_deg
-
-            # Gen index for scatter
-            index_high_deg = [[
-                ngh_high_deg_ind[row].item() for col in range(feat_high_deg.shape[1])
-            ] for row in range(ngh_high_deg_ind.shape[0])]
-
-            index_low_deg = [[ngh_low_deg_ind[row].item() for col in range(feat_low_deg.shape[1])]
-                             for row in range(ngh_low_deg_ind.shape[0])]
-
-            index_high_deg = th.tensor(index_high_deg)
-            index_low_deg = th.tensor(index_low_deg)
-
-            # Update feat of the nodes in the high and low degree
-            # self.feat.scatter(0, index_high_deg, feat_high_deg)
-            self.feat.scatter(0, index_high_deg, feat_high_deg_golden)
-
-            self.feat.scatter(0, index_low_deg, feat_low_deg, reduce='add')
-            # self.feat.scatter(0, index_low_deg, feat_low_deg_updated_golden)
-
-            return self.feat
+            return rst_delta
         else:
             rst = super().forward(graph, feat)
             self.feat = nn.Parameter(rst)

examples/pytorch/gcn/train_evo.py
@@ -9,6 +9,7 @@ from dgl.data import CoraGraphDataset, CiteseerGraphDataset, PubmedGraphDataset,
 
 import util
 import time
+import random
 
 from gcn import GCN
 from gcn import GCN_delta
@@ -316,6 +317,9 @@ def main(args):
 
                 if dump_mem_trace_flag:
                     # Record mem trace
+                    mem_access_q_full_retrain = util.nodes_reindex(node_map_evo2orig,
+                                                                   mem_access_q_full_retrain)
+                    random.shuffle(mem_access_q_full_retrain)
                     util.dump_mem_trace(mem_access_q_full_retrain, trace_path_full_retrain)
                     util.dump_mem_trace(mem_access_q_full_retrain, trace_path_all_ngh)
                 mem_access_q_full_retrain = []  # Reset queue
@@ -331,6 +335,12 @@ def main(args):
 
                 if dump_mem_trace_flag:
                     # Record mem trace
+                    mem_access_q_full_retrain = util.nodes_reindex(node_map_evo2orig,
+                                                                   mem_access_q_full_retrain)
+                    mem_access_q_all_ngh = util.nodes_reindex(node_map_evo2orig,
+                                                              mem_access_q_all_ngh)
+                    random.shuffle(mem_access_q_full_retrain)
+                    random.shuffle(mem_access_q_all_ngh)
                     util.dump_mem_trace(mem_access_q_full_retrain, trace_path_full_retrain)
                     util.dump_mem_trace(mem_access_q_all_ngh, trace_path_all_ngh)
                 mem_access_q_full_retrain = []  # Reset queue
@@ -455,26 +465,14 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # args.dataset = 'cora'
-    # args.n_epochs = 200
+    # args.n_epochs = 0
     # args.deg_threshold = 300
 
-    dump_accuracy_flag = 1
-    dump_mem_trace_flag = 0
+    dump_accuracy_flag = 0
+    dump_mem_trace_flag = 1
     dump_node_access_flag = 0
 
     print('\n************ {:s} ************'.format(args.dataset))
     print(args)
-
-    # new_val = torch.arange(1, 21).reshape((4, 5))
-    # print(new_val)
-    # src = torch.ones(4, 5, dtype=new_val.dtype)
-    # row_id = th.tensor([0, 2, 3])
-    # index = [[row_id[k].item() for i in range(new_val.shape[1])] for k in range(row_id.shape[0])]
-    # print(index)
-    # index=th.tensor(index)
-
-    # # src = torch.arange(1, 11).reshape((2, 5))
-    # # index = torch.tensor([[0, 0, 0, 0, 0], [2,2,2,2, 2]])
-    # print(src.scatter_(0, index, new_val, reduce='add'))
 
     main(args)

examples/pytorch/gcn/train_evo_delta.py
@@ -9,11 +9,14 @@ from dgl.data import CoraGraphDataset, CiteseerGraphDataset, PubmedGraphDataset,
 
 import util
 import time
+import random
 
 from gcn import GCN
 from gcn import GCN_delta
 
 from torchviz import make_dot
+
+from torch.utils.tensorboard import SummaryWriter
 
 import os
 
@@ -25,62 +28,22 @@ def train(model, features, n_edges, train_mask, val_mask, labels, loss_fcn, opti
         model.train()
         if epoch >= 3:
             t0 = time.time()
+
         # forward
         logits = model(features)
         loss = loss_fcn(logits[train_mask], labels[train_mask])
-
-        # w1 = copy.deepcopy(model.layers[0].weight)
-        # print('\n>> Before train:')
-        # for param in model.parameters():
-        #     print(param)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        # print('\n>> After train:')
-        # for param in model.parameters():
-        #     print(param)
-        # w2 = model.layers[0].weight
-        # print(w1.equal(w2))
-
         if epoch >= 3:
             dur.append(time.time() - t0)
 
-        acc = evaluate(model, features, labels, val_mask)
+        # acc = evaluate(model, features, labels, val_mask)
         # print("Epoch {:05d} | Time(s) {:.4f} | Loss {:.4f} | Accuracy {:.4f} | "
         #       "ETputs(KTEPS) {:.2f}".format(epoch, np.mean(dur), loss.item(),
         #                                     acc, n_edges / np.mean(dur) / 1000))
-
-
-# def train_delta(model, features, n_edges, train_mask, val_mask, labels, loss_fcn, optimizer,
-#                 nodes_high_deg, nodes_low_deg):
-
-#     # torch.autograd.set_detect_anomaly(True)
-
-#     # initialize graph
-#     dur = []
-#     for epoch in range(args.n_epochs):
-#         model.train()
-#         if epoch >= 3:
-#             t0 = time.time()
-#         # forward
-#         logits = model(features, nodes_high_deg, nodes_low_deg)
-#         loss = loss_fcn(logits[train_mask], labels[train_mask])
-
-#         optimizer.zero_grad()
-#         # loss.backward(retain_graph=True)
-#         loss.backward()
-#         optimizer.step()
-
-#         if epoch >= 3:
-#             dur.append(time.time() - t0)
-
-#         acc = evaluate_delta_with_degree(model, features, labels, val_mask, nodes_high_deg,
-#                                          nodes_low_deg)
-#         # print("Epoch {:05d} | Time(s) {:.4f} | Loss {:.4f} | Accuracy {:.4f} | "
-#         #       "ETputs(KTEPS) {:.2f}".format(epoch, np.mean(dur), loss.item(),
-#         #                                     acc, n_edges / np.mean(dur) / 1000))
 
 
 def train_delta_edge_masked(model,
@@ -93,6 +56,7 @@ def train_delta_edge_masked(model,
                             optimizer,
                             ngh_high_deg=None,
                             ngh_low_deg=None):
+
     # initialize graph
     dur = []
     for epoch in range(args.n_epochs):
@@ -103,11 +67,15 @@ def train_delta_edge_masked(model,
         logits = model(features, ngh_high_deg, ngh_low_deg)
         loss = loss_fcn(logits[train_mask], labels[train_mask])
 
-        with torch.autograd.set_detect_anomaly(True):
-            optimizer.zero_grad()
-            # loss.backward(retain_graph=True)
-            loss.backward()
-            optimizer.step()
+        # print('\n>> Grad', [x.grad for x in optimizer.param_groups[0]['params']])
+
+        # print()
+        # for name, parameters in model.named_parameters():
+        #     print('\n' + name, ':', parameters)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
         if epoch >= 3:
             dur.append(time.time() - t0)
@@ -115,8 +83,11 @@ def train_delta_edge_masked(model,
         acc = evaluate_delta_edge_masked(model, features, labels, val_mask, ngh_high_deg,
                                          ngh_low_deg)
         # print("Epoch {:05d} | Time(s) {:.4f} | Loss {:.4f} | Accuracy {:.4f} | "
-        #       "ETputs(KTEPS) {:.2f}".format(epoch, np.mean(dur), loss.item(),
-        #                                     acc, n_edges / np.mean(dur) / 1000))
+        #       "ETputs(KTEPS) {:.2f}".format(epoch, np.mean(dur), loss.item(), acc,
+        #                                     n_edges / np.mean(dur) / 1000))
+
+    # Update embedding
+    model.embedding = torch.nn.Parameter(logits)
 
 
 def evaluate(model, features, labels, mask):
@@ -149,17 +120,6 @@ def evaluate_delta_edge_masked(model,
         _, indices = torch.max(logits, dim=1)
         correct = torch.sum(indices == labels)
         return correct.item() * 1.0 / len(labels)
-
-
-# def evaluate_delta_edge_masked(model, features, labels, mask):
-#     model.eval()
-#     with torch.no_grad():
-#         logits = model(features)
-#         logits = logits[mask]
-#         labels = labels[mask]
-#         _, indices = torch.max(logits, dim=1)
-#         correct = torch.sum(indices == labels)
-#         return correct.item() * 1.0 / len(labels)
 
 
 def main(args):
@@ -339,6 +299,14 @@ def main(args):
 
         print('Add node size: ', len(inserted_nodes))
 
+        # Execute full retraining at the beginning
+        if i <= 0:
+            util.graph_evolve(inserted_nodes, g_csr, g, node_map_orig2evo, node_map_evo2orig,
+                              model_delta_all_ngh.g)
+        else:
+            util.graph_evolve_delta_all_ngh(inserted_nodes, g_csr, g, node_map_orig2evo,
+                                            node_map_evo2orig, model_delta_all_ngh.g)
+
         # Get node index of added_nodes in evolve graph
         inserted_nodes_evo = util.nodes_reindex(node_map_orig2evo, inserted_nodes)
         # inserted_nodes_evo.sort()
@@ -364,6 +332,8 @@ def main(args):
 
         if dump_mem_trace_flag:
             # Record mem trace
+            mem_access_q_delta_ngh = util.nodes_reindex(node_map_evo2orig, mem_access_q_delta_ngh)
+            random.shuffle(mem_access_q_delta_ngh)
             util.dump_mem_trace(mem_access_q_delta_ngh, trace_path_delta_ngh)
         mem_access_q_delta_ngh = []  # Reset queue
 
@@ -379,14 +349,6 @@ def main(args):
             # Delta retraining on delta neighbors
             """
             print('\n>> Delta neighbor retraining')
-            # Execute full retraining at the beginning
-            if i <= 0:
-                util.graph_evolve(inserted_nodes, g_csr, g, node_map_orig2evo, node_map_evo2orig,
-                                  model_delta_all_ngh.g)
-            else:
-                util.graph_evolve_delta_all_ngh(inserted_nodes, g_csr, g, node_map_orig2evo,
-                                                node_map_evo2orig, model_delta_all_ngh.g)
-
             # Get ngh with high deg and low deg
             ngh_high_deg, ngh_low_deg = util.get_ngh_with_deg_th(model_delta_all_ngh.g,
                                                                  inserted_nodes_evo, deg_th)
@@ -408,16 +370,11 @@ def main(args):
 
             if len(node_q) > 0:
                 time_start = time.perf_counter()
-                # train_delta(model_delta_all_ngh, features, model_delta_all_ngh.g.number_of_edges(),
-                #             train_mask, val_mask, labels, loss_fcn_delta_all_ngh,
-                #             optimizer_delta_all_ngh, ngh_high_deg, ngh_low_deg)
-
-                # train(model_delta_all_ngh, features, model_delta_all_ngh.g.number_of_edges(),
-                #       train_mask, val_mask, labels, loss_fcn_delta_all_ngh, optimizer_delta_all_ngh)
-                if i <= 3:
-                    train(model_delta_all_ngh, features, model_delta_all_ngh.g.number_of_edges(),
-                          train_mask, val_mask, labels, loss_fcn_delta_all_ngh,
-                          optimizer_delta_all_ngh)
+                if i <= -1:
+                    train_delta_edge_masked(model_delta_all_ngh, features,
+                                            model_delta_all_ngh.g.number_of_edges(), train_mask,
+                                            val_mask, labels, loss_fcn_delta_all_ngh,
+                                            optimizer_delta_all_ngh)
                 else:
                     train_delta_edge_masked(model_delta_all_ngh, features,
                                             model_delta_all_ngh.g.number_of_edges(), train_mask,
@@ -429,13 +386,9 @@ def main(args):
                 print('>> Epoch training time in delta ngh: {:.4}s'.format(
                     time_delta_all_ngh_retrain))
 
-            if i <= -1:
+            if i <= 8:
                 acc = evaluate(model_delta_all_ngh, features, labels, test_mask)
             else:
-                # acc = evaluate_delta_with_degree(model_delta_all_ngh, features, labels, test_mask,
-                #                                  ngh_high_deg, ngh_low_deg)
-                # acc = evaluate_delta_with_degree(model_delta_all_ngh, features, labels, test_mask)
-
                 acc = evaluate_delta_edge_masked(model_delta_all_ngh, features, labels, test_mask,
                                                  list(ngh_high_deg.keys()),
                                                  list(ngh_low_deg.keys()))
@@ -492,7 +445,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # args.dataset = 'cora'
-    # args.n_epochs = 0
+    # args.n_epochs = 200
     # args.deg_threshold = 5
 
     dump_accuracy_flag = 1