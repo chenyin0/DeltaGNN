8c4a3194df9ac59124a69e99d9331b488d09eeb4
yc <yinchen0621@foxmail.com>
2022-10-10 10:27:23 +0800

    [gcn] remove igraph lib in plt_graph

--- examples/pytorch/gcn/plt_graph.py
+++ examples/pytorch/gcn/plt_graph.py
@@ -1,49 +1,48 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import igraph as ig
+# import igraph as ig
 from scipy.sparse import csr_matrix
 import time
 from brokenaxes import brokenaxes
 from matplotlib import ticker
 from matplotlib.pyplot import MultipleLocator
 
+# def graph_visualize(indptr, indices, data):
+#     print(">>> Graph visualize")
+#     begin = time.time()
+#     if (data is None):
+#         data = np.ones(len(indices))
 
-def graph_visualize(indptr, indices, data):
-    print(">>> Graph visualize")
-    begin = time.time()
-    if (data is None):
-        data = np.ones(len(indices))
+#     mat_size = len(indptr) - 1
+#     adj = csr_matrix((data, indices, indptr), shape=(mat_size, mat_size))
 
-    mat_size = len(indptr) - 1
-    adj = csr_matrix((data, indices, indptr), shape=(mat_size, mat_size))
+#     # g = igraph.Graph()
+#     # print(adj.toarray())
+#     g = ig.Graph.Adjacency(adj.toarray(), mode="undirected")
+#     # print(g)
+#     # g.Adjacency(adj, mode="directed")
+#     # g._graph_from_sparse_matrix(adj, mode="directed")
 
-    # g = igraph.Graph()
-    # print(adj.toarray())
-    g = ig.Graph.Adjacency(adj.toarray(), mode="undirected")
-    # print(g)
-    # g.Adjacency(adj, mode="directed")
-    # g._graph_from_sparse_matrix(adj, mode="directed")
+#     ig.Graph.community_fastgreedy(g)
+#     layout = g.layout("drl")
 
-    ig.Graph.community_fastgreedy(g)
-    layout = g.layout("drl")
+#     import matplotlib.pyplot as plt
+#     print(">>> Plot fig")
+#     fig, ax = plt.subplots()
+#     ig.plot(g, target=ax, layout=layout)
+#     # plt.savefig("test.svg")
+#     end = time.time()
+#     print("Time cost: ", end - begin)
+#     plt.show()
 
-    import matplotlib.pyplot as plt
-    print(">>> Plot fig")
-    fig, ax = plt.subplots()
-    ig.plot(g, target=ax, layout=layout)
-    # plt.savefig("test.svg")
-    end = time.time()
-    print("Time cost: ", end - begin)
-    plt.show()
+#     # img.save("img.pdf")
 
-    # img.save("img.pdf")
+#     # fig, ax = plt.subplots()
+#     # ig.plot(g, layout=layout, target=ax)
+#     # ig.plot(g, layout=layout)
 
-    # fig, ax = plt.subplots()
-    # ig.plot(g, layout=layout, target=ax)
-    # ig.plot(g, layout=layout)
-
-    # igraph.plot(g, layout)
-    # fig.show()
+#     # igraph.plot(g, layout)
+#     # fig.show()
 
 
 def plot_degree_distribution(degree_list):
@@ -55,7 +54,7 @@ def plot_degree_distribution(degree_list):
     y = [degree_list[i] / node_totol_num for i in range(len(degree_list))]
     # y = [degree_list[i] for i in range(len(degree_list))]
 
-    fig = plt.figure(figsize=(3,4))
+    fig = plt.figure(figsize=(3, 4))
     bax = brokenaxes(xlims=((0, 22), (165, 170)), hspace=1, despine=False)
     bax.bar(x, y)
 
