703d4d5651635a9ebf8be83cdaa9667304652201
yc <yinchen0621@foxmail.com>
2023-02-27 07:50:25 +0800

    [GCN] 1. Remove BatchNorm layer       2. Modify update delta

--- examples/pytorch/delta_gnn/model/gcn_t.py
+++ examples/pytorch/delta_gnn/model/gcn_t.py
@@ -19,27 +19,38 @@ import numpy as np
 import time
 import util
 from torch_sparse import SparseTensor
+from tqdm import tqdm
 
 
-class GCN_t(nn.Module):
+class GCN(nn.Module):
 
     def __init__(self, in_feats, n_hidden, n_classes, n_layers, dropout):
-        super(GCN_t, self).__init__()
+        super(GCN, self).__init__()
         self.layers = nn.ModuleList()
         self.bns = nn.ModuleList()
 
+        # if n_layers > 1:
+        #     self.layers.append(GCNConv(in_feats, n_hidden))
+        #     self.bns.append(nn.BatchNorm1d(n_hidden))
+        #     for i in range(1, n_layers - 1):
+        #         self.layers.append(GCNConv(n_hidden, n_hidden))
+        #         self.bns.append(nn.BatchNorm1d(n_hidden))
+        #     self.layers.append(GCNConv(n_hidden, n_classes))
+        # else:
+        #     self.layers.append(GCNConv(in_feats, n_classes))
+
+        # self.dropout = dropout
+        # # self.logits = torch.zeros(g.number_of_nodes(), n_classes)
+
         if n_layers > 1:
             self.layers.append(GCNConv(in_feats, n_hidden))
-            self.bns.append(nn.BatchNorm1d(n_hidden))
             for i in range(1, n_layers - 1):
                 self.layers.append(GCNConv(n_hidden, n_hidden))
-                self.bns.append(nn.BatchNorm1d(n_hidden))
             self.layers.append(GCNConv(n_hidden, n_classes))
         else:
             self.layers.append(GCNConv(in_feats, n_classes))
 
         self.dropout = dropout
-        # self.logits = torch.zeros(g.number_of_nodes(), n_classes)
 
     def reset_parameters(self):
         for layer in self.layers:
@@ -47,20 +58,30 @@ class GCN_t(nn.Module):
         for bn in self.bns:
             bn.reset_parameters()
 
-    def forward(self, features, adj_t):
+    # def forward(self, features, adj_t):
+    #     h = features
+    #     for i, layer in enumerate(self.layers[:-1]):
+    #         h = layer(h, adj_t)
+    #         h = self.bns[i](h)
+    #         h = F.relu(h)
+    #         h = F.dropout(h, p=self.dropout, training=self.training)
+    #     h = self.layers[-1](h, adj_t)
+    #     return h.log_softmax(dim=-1)
+
+    def forward(self, features, edge_index):
         h = features
         for i, layer in enumerate(self.layers[:-1]):
-            h = layer(h, adj_t)
-            h = self.bns[i](h)
+            h = layer(h, edge_index)
             h = F.relu(h)
             h = F.dropout(h, p=self.dropout, training=self.training)
-        h = self.layers[-1](h, adj_t)
+        h = self.layers[-1](h, edge_index)
         return h.log_softmax(dim=-1)
 
 
 def train(model, device, train_loader, lr, weight_decay):
     model.train()
-    # adj_t = adj_t.to(device)
+    # pbar = tqdm(total=int(len(train_loader.dataset)))
+    # pbar.set_description(f'Epoch {epoch:02d}')
     optimizer = Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
     time_epoch = 0
     loss_list = []
@@ -75,6 +96,7 @@ def train(model, device, train_loader, lr, weight_decay):
         time_epoch += (time.time() - t_st)
         loss_list.append(loss.item())
         # print(loss.item())
+        # pbar.update(batch.batch_size)
     return np.mean(loss_list), time_epoch
 
 
@@ -86,7 +108,8 @@ def validate(model, device, loader):
     for step, batch in enumerate(loader):
         x, edge_index, y = batch.x.to(device), batch.edge_index.to(device), batch.y
         out = model(x, edge_index)
-        y_pred.append(torch.argmax(out, dim=1, keepdim=True).cpu())
+        # y_pred.append(torch.argmax(out, dim=1, keepdim=True).cpu())
+        y_pred.append(torch.argmax(out, dim=-1, keepdim=True).cpu())
         y_true.append(y)
     y_pred = torch.cat(y_pred, dim=0)
     y_true = torch.cat(y_true, dim=0)
@@ -103,7 +126,8 @@ def test(model, device, loader, checkpt_file):
     for step, batch in enumerate(loader):
         x, edge_index, y = batch.x.to(device), batch.edge_index.to(device), batch.y
         out = model(x, edge_index)
-        y_pred.append(torch.argmax(out, dim=1, keepdim=True).cpu())
+        # y_pred.append(torch.argmax(out, dim=1, keepdim=True).cpu())
+        y_pred.append(torch.argmax(out, dim=-1, keepdim=True).cpu())
         y_true.append(y)
     y_pred = torch.cat(y_pred, dim=0)
     y_true = torch.cat(y_true, dim=0)
@@ -152,7 +176,7 @@ class GCN_delta(nn.Module):
         in_feats: feature matrix of current subgraph
     """
 
-    def __init__(self, g, in_feats, n_hidden, n_classes, n_layers, activation, dropout):
+    def __init__(self, g, in_feats, n_hidden, n_classes, n_layers, dropout):
         super(GCN_delta, self).__init__()
         self.g = g
         self.layers = nn.ModuleList()
@@ -192,16 +216,13 @@ class GCN_delta(nn.Module):
         self.dropout = dropout
 
         # Record previous embedding
-        # self.embedding = th.Tensor([[0 for i in range(n_classes)]
-        #                             for j in range(g.number_of_nodes())]).requires_grad_(True)
-        self.embedding = []
-        for i in range(n_layers - 1):
-            self.embedding.append(
-                th.Tensor([[0 for i in range(n_hidden)]
-                           for j in range(g.number_of_nodes())]).requires_grad_(True))
-        self.embedding.append(
-            th.Tensor([[0 for i in range(n_classes)]
-                       for j in range(g.number_of_nodes())]).requires_grad_(True))
+        self.embedding = th.Tensor([[0 for i in range(n_classes)]
+                                    for j in range(g.number_of_nodes())]).requires_grad_(True)
+        # self.embedding = []
+        # for i in range(n_layers - 1):
+        #     self.embedding.append(
+        #         th.Tensor([[0 for i in range(n_hidden)]
+        #                    for j in range(g.number_of_nodes())]).requires_grad_(True))
 
     def reset_parameters(self):
         for layer in self.layers:
@@ -223,14 +244,14 @@ class GCN_delta(nn.Module):
 
     #     return h
 
-    def forward(self, features, adj_t, v_sensitive, v_insensitive):
+    def forward(self, features, edge_index, v_sensitive, v_insensitive):
         h = features
         for i, layer in enumerate(self.layers[:-1]):
-            h = layer(h, adj_t)
+            h = layer(h, edge_index)
             h = self.bns[i](h)
             h = F.relu(h)
             h = F.dropout(h, p=self.dropout, training=self.training)
-        h = self.layers[-1](h, adj_t)
+        h = self.layers[-1](h, edge_index)
 
         # if not self.training:
         if v_sensitive is not None or v_insensitive is not None:
@@ -412,6 +433,64 @@ class GCN_delta(nn.Module):
         return feat
 
 
+def train_delta(model, device, train_loader, lr, weight_decay, v_sen=None, v_insen=None):
+    print('>> Start training')
+    model.train()
+    optimizer = Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+
+    time_epoch = 0
+    loss_list = []
+    for step, batch in enumerate(train_loader):
+        t_st = time.time()
+        x, edge_index, y = batch.x.to(device), batch.edge_index.to(device), batch.y.to(device)
+        optimizer.zero_grad()
+        out, feat = model(x, edge_index, v_sen, v_insen)
+        loss = F.nll_loss(out, y.squeeze(1))
+        loss.backward()
+        optimizer.step()
+        time_epoch += (time.time() - t_st)
+        loss_list.append(loss.item())
+        # print(loss.item())
+        # pbar.update(batch.batch_size)
+
+    # Update embedding
+    model.embedding = torch.nn.Parameter(feat)
+    return np.mean(loss_list), time_epoch
+
+
+@torch.no_grad()
+def validate_delta(model, device, loader, v_sen=None, v_insen=None):
+    model.eval()
+    y_pred, y_true = [], []
+    for step, batch in enumerate(loader):
+        x, edge_index, y = batch.x.to(device), batch.edge_index.to(device), batch.y
+        out, _ = model(x, edge_index, v_sen, v_insen)
+        # y_pred.append(torch.argmax(out, dim=1, keepdim=True).cpu())
+        y_pred.append(torch.argmax(out, dim=-1, keepdim=True).cpu())
+        y_true.append(y)
+    y_pred = torch.cat(y_pred, dim=0)
+    y_true = torch.cat(y_true, dim=0)
+    correct = torch.sum(y_pred == y_true)
+    return correct.item() * 1.0 / len(y_true)
+
+
+@torch.no_grad()
+def test_delta(model, device, loader, checkpt_file, v_sen=None, v_insen=None):
+    model.load_state_dict(torch.load(checkpt_file))
+    model.eval()
+    y_pred, y_true = [], []
+    for step, batch in enumerate(loader):
+        x, edge_index, y = batch.x.to(device), batch.edge_index.to(device), batch.y
+        out, _ = model(x, edge_index, v_sen, v_insen)
+        # y_pred.append(torch.argmax(out, dim=1, keepdim=True).cpu())
+        y_pred.append(torch.argmax(out, dim=-1, keepdim=True).cpu())
+        y_true.append(y)
+    y_pred = torch.cat(y_pred, dim=0)
+    y_true = torch.cat(y_true, dim=0)
+    correct = torch.sum(y_pred == y_true)
+    return correct.item() * 1.0 / len(y_true)
+
+
 # def train_delta_edge_masked(args,
 #                             model,
 #                             device,
@@ -521,67 +600,65 @@ class GCN_delta(nn.Module):
 #     # Update embedding
 #     model.embedding = [torch.nn.Parameter(i) for i in feat]
 
+# def train_delta_update(args,
+#                        model,
+#                        device,
+#                        lr,
+#                        weight_decay,
+#                        graph_adj_t=None,
+#                        v_sensitive=None,
+#                        v_insensitive=None):
+#     print('>> Start training')
+#     time_start = time.perf_counter()
+#     g = model.g
+#     features = g.ndata['feat']
+#     train_mask = g.ndata['train_mask'].bool()
+#     val_mask = g.ndata['val_mask']
+#     labels = g.ndata['label']
+#     n_edges = g.number_of_edges()
+#     # edge_mask = g.edata['edge_mask']
+#     if graph_adj_t == None:
+#         adj = SparseTensor(row=g.all_edges()[0],
+#                            col=g.all_edges()[1],
+#                            sparse_sizes=(g.number_of_nodes(), g.number_of_nodes()))
+#         adj_t = adj.to_symmetric().to(device)
+#     else:
+#         adj_t = graph_adj_t.to(device)
 
-def train_delta_update(args,
-                       model,
-                       device,
-                       lr,
-                       weight_decay,
-                       graph_adj_t=None,
-                       v_sensitive=None,
-                       v_insensitive=None):
-    print('>> Start training')
-    time_start = time.perf_counter()
-    g = model.g
-    features = g.ndata['feat']
-    train_mask = g.ndata['train_mask'].bool()
-    val_mask = g.ndata['val_mask']
-    labels = g.ndata['label']
-    n_edges = g.number_of_edges()
-    # edge_mask = g.edata['edge_mask']
-    if graph_adj_t == None:
-        adj = SparseTensor(row=g.all_edges()[0],
-                           col=g.all_edges()[1],
-                           sparse_sizes=(g.number_of_nodes(), g.number_of_nodes()))
-        adj_t = adj.to_symmetric().to(device)
-    else:
-        adj_t = graph_adj_t.to(device)
+#     optimizer = Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+#     # initialize graph
+#     loss_log = []
+#     dur = []
 
-    optimizer = Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
-    # initialize graph
-    loss_log = []
-    dur = []
+#     for epoch in range(args.n_epochs):
+#         model.train()
+#         if epoch >= 3:
+#             t0 = time.time()
+#         # forward
+#         logits, feat = model(features, adj_t, v_sensitive, v_insensitive)
+#         loss = F.cross_entropy(logits[train_mask], labels[train_mask])
 
-    for epoch in range(args.n_epochs):
-        model.train()
-        if epoch >= 3:
-            t0 = time.time()
-        # forward
-        logits, feat = model(features, adj_t, v_sensitive, v_insensitive)
-        loss = F.cross_entropy(logits[train_mask], labels[train_mask])
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+#         if epoch >= 3:
+#             dur.append(time.time() - t0)
+#         # acc = evaluate_delta_edge_masked(model, val_mask, device, ngh_high_deg, ngh_low_deg)
+#         # print("Epoch {:05d} | Time(s) {:.4f} | Loss {:.4f} | Accuracy {:.4f} | "
+#         #       "ETputs(KTEPS) {:.2f}".format(epoch, np.mean(dur), loss.item(), acc,
+#         #                                     n_edges / np.mean(dur) / 1000))
 
-        if epoch >= 3:
-            dur.append(time.time() - t0)
-        # acc = evaluate_delta_edge_masked(model, val_mask, device, ngh_high_deg, ngh_low_deg)
-        # print("Epoch {:05d} | Time(s) {:.4f} | Loss {:.4f} | Accuracy {:.4f} | "
-        #       "ETputs(KTEPS) {:.2f}".format(epoch, np.mean(dur), loss.item(), acc,
-        #                                     n_edges / np.mean(dur) / 1000))
+#         loss_log.append(round(loss.item(), 2))
+#     np.savetxt('../../../results/loss/' + args.dataset + '_evo_delta_loss' + '.txt',
+#                loss_log,
+#                fmt='%.2f')
 
-        loss_log.append(round(loss.item(), 2))
-    np.savetxt('../../../results/loss/' + args.dataset + '_evo_delta_loss' + '.txt',
-               loss_log,
-               fmt='%.2f')
+#     acc = evaluate_delta_update(model, val_mask, device, adj_t, v_sensitive, v_insensitive)
+#     print("Training time(s) {:.4f} | Accuracy {:.4f}".format(time.perf_counter() - time_start, acc))
 
-    acc = evaluate_delta_update(model, val_mask, device, adj_t, v_sensitive, v_insensitive)
-    print("Training time(s) {:.4f} | Accuracy {:.4f}".format(time.perf_counter() - time_start, acc))
-
-    # Update embedding
-    model.embedding = torch.nn.Parameter(feat)
-
+#     # Update embedding
+#     model.embedding = torch.nn.Parameter(feat)
 
 # def evaluate_delta_update(model, mask, device, v_sensitive=None, v_insensitive=None):
 #     r"""
@@ -608,42 +685,42 @@ def train_delta_update(args,
 #         return correct.item() * 1.0 / len(labels)
 
 
-def evaluate_delta_update(model,
-                          mask,
-                          device,
-                          graph_adj_t=None,
-                          v_sensitive=None,
-                          v_insensitive=None):
-    r"""
-    Update feature of updated nodes according to node degree
-    "model" should be GCN_delta
-    """
-    print('>> Start eval')
-    time_start = time.perf_counter()
-    g = model.g
-    features = g.ndata['feat']
-    labels = g.ndata['label']
-    mask = mask.bool()  # Convert int8 to bool
-    if graph_adj_t == None:
-        adj = SparseTensor(row=g.all_edges()[0],
-                           col=g.all_edges()[1],
-                           sparse_sizes=(g.number_of_nodes(), g.number_of_nodes()))
-        adj_t = adj.to_symmetric().to(device)
-    else:
-        adj_t = graph_adj_t.to(device)
+# def evaluate_delta_update(model,
+#                           mask,
+#                           device,
+#                           graph_adj_t=None,
+#                           v_sensitive=None,
+#                           v_insensitive=None):
+#     r"""
+#     Update feature of updated nodes according to node degree
+#     "model" should be GCN_delta
+#     """
+#     print('>> Start eval')
+#     time_start = time.perf_counter()
+#     g = model.g
+#     features = g.ndata['feat']
+#     labels = g.ndata['label']
+#     mask = mask.bool()  # Convert int8 to bool
+#     if graph_adj_t == None:
+#         adj = SparseTensor(row=g.all_edges()[0],
+#                            col=g.all_edges()[1],
+#                            sparse_sizes=(g.number_of_nodes(), g.number_of_nodes()))
+#         adj_t = adj.to_symmetric().to(device)
+#     else:
+#         adj_t = graph_adj_t.to(device)
 
-    model.eval()
-    with torch.no_grad():
-        logits, feat = model(features, adj_t, v_sensitive, v_insensitive)
-        # logits = model.embedding
-        logits = logits[mask]
-        labels = labels[mask]
-        _, indices = torch.max(logits, dim=1)
-        correct = torch.sum(indices == labels)
-        # # Update embedding
-        # model.embedding = [torch.nn.Parameter(i) for i in feat]
-        print("Evaluate time(s) {:.4f}".format(time.perf_counter() - time_start))
-        return correct.item() * 1.0 / len(labels)
+#     model.eval()
+#     with torch.no_grad():
+#         logits, feat = model(features, adj_t, v_sensitive, v_insensitive)
+#         # logits = model.embedding
+#         logits = logits[mask]
+#         labels = labels[mask]
+#         _, indices = torch.max(logits, dim=1)
+#         correct = torch.sum(indices == labels)
+#         # # Update embedding
+#         # model.embedding = [torch.nn.Parameter(i) for i in feat]
+#         print("Evaluate time(s) {:.4f}".format(time.perf_counter() - time_start))
+#         return correct.item() * 1.0 / len(labels)
 
 
 # def evaluate_delta_update(model,
