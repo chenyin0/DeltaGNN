cceb02639a58bfb418b405b8f7ae4c9a953b8ca9
yc <yinchen0621@foxmail.com>
2023-02-27 07:49:43 +0800

    [ogb_exp] Refactor

--- examples/pytorch/delta_gnn/ogb_exp.py
+++ examples/pytorch/delta_gnn/ogb_exp.py
@@ -13,8 +13,8 @@ from torch.utils.data import Dataset, DataLoader
 from ogb.nodeproppred import Evaluator
 from utils import SimpleDataset
 # from model import ClassMLP
-from model.gcn_t import GCN_t
-import model.gcn_t as gcn_t
+from model.gcn_t import GCN
+import model.gcn_t as gcn
 from utils import *
 from glob import glob
 import copy as cp
@@ -47,10 +47,10 @@ def main():
     parser.add_argument('--gpu', type=int, default=-1, help='gpu')
     args = parser.parse_args()
 
-    # args.dataset = 'Cora'
+    args.dataset = 'Cora'
     # args.dataset = 'CiteSeer'
     # args.dataset = 'PubMed'
-    args.dataset = 'arxiv'
+    # args.dataset = 'arxiv'
     # args.dataset = 'products'
 
     args.layer = 2
@@ -90,54 +90,38 @@ def main():
     n_layers = args.layer
     dropout = args.dropout
 
-    model_wo_retrain = GCN_t(in_feats, n_hidden, n_classes, n_layers, dropout).cuda(device)
-    model_retrain = GCN_t(in_feats, n_hidden, n_classes, n_layers, dropout).cuda(device)
-    model_delta = GCN_t(in_feats, n_hidden, n_classes, n_layers, dropout).cuda(device)
+    model_wo_retrain = GCN(in_feats, n_hidden, n_classes, n_layers, dropout).cuda(device)
+    model_retrain = GCN(in_feats, n_hidden, n_classes, n_layers, dropout).cuda(device)
+    model_delta = GCN(in_feats, n_hidden, n_classes, n_layers, dropout).cuda(device)
 
     accuracy = []
     print('Edges_init: ', edge_idx_init.shape)
     # Training for the initial snapshot
     num_nghs = [-1, -1]
-    kwargs = {
-        'features': features,
-        'labels': labels,
-        'num_nghs': num_nghs,
-        'args': args,
-        'device': device
-    }
+
+    edge_index_evolved = edge_idx_init.clone().detach()
+    train_loader, valid_loader, test_loader = gen_dataloader(args, edge_index_evolved, train_idx,
+                                                             val_idx, test_idx, features, labels,
+                                                             num_nghs)
     # Without retrain
     print('--- Model_wo_retrain:')
-    edge_index_wo_retrain = edge_idx_init.clone().detach()
-    train(model_wo_retrain,
-          edge_index_wo_retrain,
-          train_idx,
-          val_idx,
-          **kwargs,
-          checkpt_file=checkpt_file_wo_retrain)
-    acc_wo_retrain = test(model_wo_retrain,
-                          edge_index_wo_retrain,
-                          test_idx,
-                          **kwargs,
-                          checkpt_file=checkpt_file_wo_retrain)
+    # edge_index_wo_retrain = edge_idx_init.clone().detach()
+    train(args, model_wo_retrain, train_loader, valid_loader, device, checkpt_file_wo_retrain)
+    acc_wo_retrain = test(model_wo_retrain, test_loader, device, checkpt_file_wo_retrain)
 
     # Retrain
     print('--- Model_retrain:')
-    edge_index_retrain = edge_idx_init.clone().detach()
-    train(model_retrain,
-          edge_index_retrain,
-          train_idx,
-          val_idx,
-          **kwargs,
-          checkpt_file=checkpt_file_retrain)
-    acc_retrain = test(model_retrain,
-                       edge_index_retrain,
-                       test_idx,
-                       **kwargs,
-                       checkpt_file=checkpt_file_retrain)
+    # edge_index_retrain = edge_idx_init.clone().detach()
+    train(args, model_retrain, train_loader, valid_loader, device, checkpt_file_retrain)
+    acc_retrain = test(model_retrain, test_loader, device, checkpt_file_retrain)
 
     # Delta-retrain
+    print('--- Model_retrain_delta:')
+    edge_index_delta = edge_idx_init.clone().detach()
+    train(args, model_delta, train_loader, valid_loader, device, checkpt_file_delta)
+    acc_delta = test(model_delta, test_loader, device, checkpt_file_delta)
 
-    accuracy.append([0, acc_wo_retrain, acc_retrain])
+    accuracy.append([0, acc_wo_retrain, acc_retrain, acc_delta])
 
     print('------------------ update -------------------')
     snapList = [f for f in glob('./data/' + args.dataset + '/*Edgeupdate_snap*.txt')]
@@ -147,48 +131,45 @@ def main():
         print('Snapshot: {:d}'.format(i + 1))
         # Update edge_index according to inserted edges
         inserted_edge_index = load_updated_edges(args.dataset, i + 1)
-
-        # if i != 15:
-        #     args.epochs = 1
-        # else:
-        #     args.epochs = 200
-
+        edge_index_evolved = insert_edges(edge_index_evolved, inserted_edge_index)
+        train_loader, valid_loader, test_loader = gen_dataloader(args, edge_index_evolved,
+                                                                 train_idx, val_idx, test_idx,
+                                                                 features, labels, num_nghs)
         print('--- Model_wo_retrain:')
-        edge_index_wo_retrain = insert_edges(edge_index_wo_retrain, inserted_edge_index)
-        print('Edges_wo_retrain: ', edge_index_wo_retrain.shape)
-        acc_wo_retrain = test(model_wo_retrain,
-                              edge_index_wo_retrain,
-                              test_idx,
-                              **kwargs,
-                              checkpt_file=checkpt_file_wo_retrain)
+        # edge_index_wo_retrain = insert_edges(edge_index_wo_retrain, inserted_edge_index)
+        print('Edges_wo_retrain: ', edge_index_evolved.shape)
+        acc_wo_retrain = test(model_wo_retrain, test_loader, device, checkpt_file_wo_retrain)
 
         print('--- Model_retrain:')
-        edge_index_retrain = insert_edges(edge_index_retrain, inserted_edge_index)
-        print('Edges_retrain: ', edge_index_retrain.shape)
-        train(model_retrain,
-              edge_index_retrain,
-              train_idx,
-              val_idx,
-              **kwargs,
-              checkpt_file=checkpt_file_retrain)
-        acc_retrain = test(model_retrain,
-                           edge_index_retrain,
-                           test_idx,
-                           **kwargs,
-                           checkpt_file=checkpt_file_retrain)
+        # edge_index_retrain = insert_edges(edge_index_retrain, inserted_edge_index)
+        print('Edges_retrain: ', edge_index_evolved.shape)
+        train(args, model_retrain, train_loader, valid_loader, device, checkpt_file_retrain)
+        acc_retrain = test(model_retrain, test_loader, device, checkpt_file_retrain)
 
-        accuracy.append([i + 1, acc_wo_retrain, acc_retrain])
+        # print('--- Model_delta:')
+        # threshold = 0
+        # edge_index_delta, v_sen, v_insen = insert_edges_delta(edge_index_delta, inserted_edge_index,
+        #                                                       threshold)
+        # train_loader_delta, valid_loader_delta, test_loader_delta = gen_dataloader(
+        #     args, edge_index_delta, train_idx, val_idx, test_idx, features, labels, num_nghs)
+        # print('Edges_delta: ', edge_index_delta.shape)
+        # train_delta(model_delta, train_loader_delta, valid_loader_delta, device, checkpt_file_delta,
+        #             v_sen, v_insen)
+        # acc_delta = test_delta(model_delta, test_loader_delta, device, checkpt_file_delta, v_sen,
+        #                        v_insen)
+        acc_delta = 0
+
+        accuracy.append([i + 1, acc_wo_retrain, acc_retrain, acc_delta])
 
     for i in range(len(accuracy)):
-        print('{:d}\t{:.2f}  {:.2f}'.format(i, accuracy[i][1] * 100, accuracy[i][2] * 100))
+        print('{:d}\t{:.2f}  {:.2f}  {:.2f}'.format(i, accuracy[i][1] * 100, accuracy[i][2] * 100,
+                                                    accuracy[i][3] * 100))
 
     print('\n>> Task {:s} execution time: {}'.format(
         args.dataset, util.time_format(time.perf_counter() - Task_time_start)))
 
 
-def train(model, edge_index, train_idx, val_idx, features, labels, num_nghs, args, device,
-          checkpt_file):
-    model.reset_parameters()
+def gen_dataloader(args, edge_index, train_idx, val_idx, test_idx, features, labels, num_nghs):
     features = torch.FloatTensor(features)
     data = Data(x=features, edge_index=edge_index, y=labels)
     del features
@@ -204,7 +185,15 @@ def train(model, edge_index, train_idx, val_idx, features, labels, num_nghs, arg
                                   num_neighbors=num_nghs,
                                   shuffle=False,
                                   batch_size=args.batch_size)
+    test_loader = NeighborLoader(data,
+                                 input_nodes=test_idx,
+                                 num_neighbors=num_nghs,
+                                 shuffle=False,
+                                 batch_size=args.batch_size)
+    return train_loader, valid_loader, test_loader
 
+
+def train(args, model, train_loader, valid_loader, device, checkpt_file):
     bad_counter = 0
     best = 0
     best_epoch = 0
@@ -213,9 +202,9 @@ def train(model, edge_index, train_idx, val_idx, features, labels, num_nghs, arg
     print("--------------------------")
     print("Training...")
     for epoch in range(args.epochs):
-        loss_tra, train_ep = gcn_t.train(model, device, train_loader, args.lr, args.weight_decay)
+        loss_tra, train_ep = gcn.train(model, device, train_loader, args.lr, args.weight_decay)
         t_st = time.time()
-        f1_val = gcn_t.validate(model, device, valid_loader)
+        f1_val = gcn.validate(model, device, valid_loader)
         train_time += train_ep
         if (epoch + 1) % 20 == 0:
             print('Epoch:{:02d}, Train_loss:{:.3f}, Valid_acc:{:.2f}%, Time_cost:{:.3f}/{:.3f}'.
@@ -236,22 +225,115 @@ def train(model, edge_index, train_idx, val_idx, features, labels, num_nghs, arg
     print('The best epoch: {}th'.format(best_epoch))
 
 
-def test(model, edge_index, test_idx, features, labels, num_nghs, args, device, checkpt_file):
-    features = torch.FloatTensor(features)
-    data = Data(x=features, edge_index=edge_index, y=labels)
-    del features
-    gc.collect()
-
-    test_loader = NeighborLoader(data,
-                                 input_nodes=test_idx,
-                                 num_neighbors=num_nghs,
-                                 shuffle=False,
-                                 batch_size=args.batch_size)
-
-    test_acc = gcn_t.test(model, device, test_loader, checkpt_file)
+def test(model, test_loader, device, checkpt_file):
+    test_acc = gcn.test(model, device, test_loader, checkpt_file)
     print('Test accuracy:{:.2f}%'.format(100 * test_acc))
     return test_acc
 
+
+def train_delta(args, model, train_loader, valid_loader, device, checkpt_file, v_sen, v_insen):
+    bad_counter = 0
+    best = 0
+    best_epoch = 0
+    train_time = 0
+    model.reset_parameters()
+    print("--------------------------")
+    print("Training...")
+    for epoch in range(args.epochs):
+        loss_tra, train_ep = gcn.train_delta(model, device, train_loader, args.lr,
+                                             args.weight_decay, v_sen, v_insen)
+        t_st = time.time()
+        f1_val = gcn.validate_delta(model, device, valid_loader, v_sen, v_insen)
+        train_time += train_ep
+        if (epoch + 1) % 20 == 0:
+            print('Epoch:{:02d}, Train_loss:{:.3f}, Valid_acc:{:.2f}%, Time_cost:{:.3f}/{:.3f}'.
+                  format(epoch + 1, loss_tra, 100 * f1_val, train_ep, train_time))
+            # print('Remove print')
+        if f1_val > best:
+            best = f1_val
+            best_epoch = epoch + 1
+            t_st = time.time()
+            torch.save(model.state_dict(), checkpt_file)
+            bad_counter = 0
+        else:
+            bad_counter += 1
+        if bad_counter == args.patience:
+            break
+
+    print('Train cost: {:.2f}s'.format(train_time))
+    print('The best epoch: {}th'.format(best_epoch))
+
+
+def test_delta(model, test_loader, device, checkpt_file, v_sen, v_insen):
+    test_acc = gcn.test_delta(model, device, test_loader, checkpt_file, v_sen, v_insen)
+    print('Test accuracy:{:.2f}%'.format(100 * test_acc))
+    return test_acc
+
+
+# def train(model, edge_index, train_idx, val_idx, features, labels, num_nghs, args, device,
+#           checkpt_file):
+#     model.reset_parameters()
+#     features = torch.FloatTensor(features)
+#     data = Data(x=features, edge_index=edge_index, y=labels)
+#     del features
+#     gc.collect()
+
+#     train_loader = NeighborLoader(data,
+#                                   input_nodes=train_idx,
+#                                   num_neighbors=num_nghs,
+#                                   shuffle=True,
+#                                   batch_size=args.batch_size)
+#     valid_loader = NeighborLoader(data,
+#                                   input_nodes=val_idx,
+#                                   num_neighbors=num_nghs,
+#                                   shuffle=False,
+#                                   batch_size=args.batch_size)
+
+#     bad_counter = 0
+#     best = 0
+#     best_epoch = 0
+#     train_time = 0
+#     model.reset_parameters()
+#     print("--------------------------")
+#     print("Training...")
+#     for epoch in range(args.epochs):
+#         loss_tra, train_ep = gcn.train(model, device, train_loader, args.lr, args.weight_decay)
+#         t_st = time.time()
+#         f1_val = gcn.validate(model, device, valid_loader)
+#         train_time += train_ep
+#         if (epoch + 1) % 20 == 0:
+#             print('Epoch:{:02d}, Train_loss:{:.3f}, Valid_acc:{:.2f}%, Time_cost:{:.3f}/{:.3f}'.
+#                   format(epoch + 1, loss_tra, 100 * f1_val, train_ep, train_time))
+#             # print('Remove print')
+#         if f1_val > best:
+#             best = f1_val
+#             best_epoch = epoch + 1
+#             t_st = time.time()
+#             torch.save(model.state_dict(), checkpt_file)
+#             bad_counter = 0
+#         else:
+#             bad_counter += 1
+#         if bad_counter == args.patience:
+#             break
+
+#     print('Train cost: {:.2f}s'.format(train_time))
+#     print('The best epoch: {}th'.format(best_epoch))
+
+# def test(model, edge_index, test_idx, features, labels, num_nghs, args, device, checkpt_file):
+#     features = torch.FloatTensor(features)
+#     data = Data(x=features, edge_index=edge_index, y=labels)
+#     del features
+#     gc.collect()
+
+#     test_loader = NeighborLoader(data,
+#                                  input_nodes=test_idx,
+#                                  num_neighbors=num_nghs,
+#                                  shuffle=False,
+#                                  batch_size=args.batch_size)
+
+#     test_acc = gcn.test(model, device, test_loader, checkpt_file)
+#     print('Test accuracy:{:.2f}%'.format(100 * test_acc))
+#     return test_acc
 
 if __name__ == '__main__':
     main()
